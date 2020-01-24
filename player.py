14311fdc7b965a909a6526df945ebfa898608328
nithish <nithish98raja@gmail.com>
2020-01-24 11:02:37 +0530

    add function to update player location

--- player.py
+++ player.py
@@ -6,16 +6,28 @@
 # Initialise class
 class Player:
     # Configure player
-    def __init__(self, location):
+    def __init__(self):
         """Configure player with initial values."""
         # Set player location
-        self.location = location
+        self.location = None
 
         # Set hungry status to true
         self.hungry = True
 
         # initialise target
         self.target = None
+
+    # Function to set current location
+    def setLocation(self, location):
+        """Set current location of playerself.
+
+        Keyword arguments:
+        location -- tuple with x and y coordinates
+        """
+        # Check if location is a tuple
+        if type(location) == type(tuple([1, 2])):
+            # Set current location
+            self.location = location
 
     # Function to set target
     def setTarget(self, target):
